8a39256cb5c17dce0885fc2d8a57cc1e24f43243
udaymishra04 <108628308+udaymishra04@users.noreply.github.com>
2025-10-19 00:54:44 +0530

    completed

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -7,12 +7,8 @@ import store from '../app/store.js'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-    <AuthProvider>
-    <ChatProvider>
     <Provider store={store}>
         <App />
     </Provider>
-    </ChatProvider>
-    </AuthProvider>
    </BrowserRouter>
 )
